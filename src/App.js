242b78c9158b5de876d7cda70a0ed2bd905689df
SHaider0 <Samnoon.Haider@gmail.com>
2023-05-08 22:02:32 -0400

    Add styles to nav menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/Home";
 import MyStory from "./components/MyStory";
@@ -6,7 +6,6 @@ import MyProjects from "./components/MyProjects";
 import MyTutorials from "./components/MyTutorials";
 import Contact from "./components/Contact";
 import "./App.css";
-import { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -19,7 +18,36 @@ import {
   Box,
 } from "@mui/material";
 import { Menu } from "@mui/icons-material";
-import "./App.css";
+import { styled } from "@mui/material/styles";
+
+const StyledAppBar = styled(AppBar)({
+  backgroundColor: "#333",
+  boxShadow: "none",
+  borderBottom: "1px solid #ccc",
+});
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const StyledDrawer = styled(Drawer)({
+  "& .MuiDrawer-paper": {
+    background: "linear-gradient(to bottom right, #000000, #000000, #150f26)",
+    color: "#fff",
+    paddingTop: "20px",
+  },
+});
+
+const StyledLink = styled(Link)({
+  color: "#fff",
+  textDecoration: "none",
+  fontSize: "1.2rem",
+  padding: "10px",
+  "&:hover": {
+    textDecoration: "underline",
+  },
+});
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -29,50 +57,50 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <AppBar position="static">
-          <Toolbar>
-            {/* <Typography variant="h6" component={Link} to="/">
+        <StyledAppBar position="static">
+          <StyledToolbar>
+            <Typography variant="h6" component={Link} to="/">
               SMH
-            </Typography> */}
-            <Box sx={{ flexGrow: 1 }} />
+            </Typography>
             <IconButton color="inherit" onClick={handleMenuOpen}>
               <Menu />
             </IconButton>
-            <Drawer
-              anchor="right"
-              open={menuOpen}
-              onClose={handleMenuOpen}
-              ModalProps={{ onBackdropClick: handleMenuOpen }}
-            >
-              <List>
-                <ListItem component={Link} to="/">
-                  <ListItemText onClick={handleMenuOpen} primary="Home" />
-                </ListItem>
-                <ListItem component={Link} to="/about">
-                  <ListItemText onClick={handleMenuOpen} primary="My Story" />
-                </ListItem>
-                <ListItem component={Link} to="/projects">
-                  <ListItemText
-                    onClick={handleMenuOpen}
-                    primary="My Projects"
-                  />
-                </ListItem>
-                <ListItem component={Link} to="/tutorials">
-                  <ListItemText
-                    onClick={handleMenuOpen}
-                    primary="My Tutorials"
-                  />
-                </ListItem>
-                <ListItem component={Link} to="/contact">
-                  <ListItemText
-                    onClick={handleMenuOpen}
-                    primary="Get In Touch"
-                  />
-                </ListItem>
-              </List>
-            </Drawer>
-          </Toolbar>
-        </AppBar>
+          </StyledToolbar>
+        </StyledAppBar>
+        <StyledDrawer
+          anchor="right"
+          open={menuOpen}
+          onClose={handleMenuOpen}
+          ModalProps={{ onBackdropClick: handleMenuOpen }}
+        >
+          <List>
+            <ListItem>
+              <StyledLink to="/" onClick={handleMenuOpen}>
+                Home
+              </StyledLink>
+            </ListItem>
+            <ListItem>
+              <StyledLink to="/about" onClick={handleMenuOpen}>
+                My Story
+              </StyledLink>
+            </ListItem>
+            <ListItem>
+              <StyledLink to="/projects" onClick={handleMenuOpen}>
+                My Projects
+              </StyledLink>
+            </ListItem>
+            <ListItem>
+              <StyledLink to="/tutorials" onClick={handleMenuOpen}>
+                My Tutorials
+              </StyledLink>
+            </ListItem>
+            <ListItem>
+              <StyledLink to="/contact" onClick={handleMenuOpen}>
+                Get In Touch
+              </StyledLink>
+            </ListItem>
+          </List>
+        </StyledDrawer>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<MyStory />} />
